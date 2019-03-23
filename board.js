7915bbfa21c816a8f310551499cc42e4177815ab
Sam Ventocilla <ventocis@mail.gvsu.edu>
2019-03-22 23:07:57 -0400

    commit

---
+++ board.js
@@ -0,0 +1,264 @@
+/**
+ * Board
+ * Defines a board "class" for an Othello game.
+ */
+
+module.exports = class Board {
+	/**
+	 * Construct the object with required state
+	 */
+	constructor(height, width){
+		this.height = height;
+		this.width = width;
+		this.board = [];
+		var h2 = Math.round(height/2);
+		var w2 = Math.round(width/2);
+		for(let i=0; i<this.height; ++i){
+			let tmp = [];
+			for(let j=0; j<this.width; ++j){
+				if(i == (h2-1) && j == (w2-1)){
+					tmp.push(0);
+				}
+				else if(i == h2 && j == (w2-1)){
+					tmp.push(1);
+				}
+				else if(i == (h2-1) && j == w2){
+					tmp.push(1);
+				}
+				else if(i == h2 && j == w2){
+					tmp.push(0);
+				}else{
+					tmp.push(-1);
+				}
+			}
+			this.board.push(tmp);
+		}
+	}
+
+	setBoard(tmp){
+		this.board = (tmp);
+	}
+	
+
+	/**
+	 * Print a representation of the board to the terminal.
+	 */
+	printBoard(){
+		process.stdout.write('   ');
+		for(let h=1; h <= this.width; ++h)
+			process.stdout.write(h + '       ');
+		process.stdout.write('\n');
+		for(let i=0; i<this.height; ++i){
+			let j = i+1;
+			process.stdout.write(j + '  ');
+			for(let j=0; j<this.width; ++j){
+				if(this.board[i][j] == -1){
+					process.stdout.write('-       ')
+				} else {
+					process.stdout.write(this.board[i][j] + "       ")
+				}
+			}
+			console.log();
+			console.log('\n');
+		}
+	}
+
+	/**
+	 * isValidMove
+	 * @param row An integer row number.
+	 * @param col An integer column number.
+	 * @param disc A character for the disc color.
+	 * @return A boolean indicating whether the move is valid.
+	 */
+
+	isValidMove(row, col, disc){
+
+		// Check if move is on the board
+		if (this.isOnBoard(row, col) == false)
+			return false;
+
+		// Check if move is onto an empty space
+		if(this.isOnBoard(row, col) == true)
+			if (this.board[row][col] != -1)
+				return false;
+
+		// Save the opponent color
+		var opponent = 1;
+		if (disc == opponent)
+			opponent = 0;
+		// Check if move captures opponent piece in any direction
+		for (var i = -1; i < 2; i++)
+			for (var j = -1; j < 2; j++) {
+				var searchR = (row + i);
+				var searchC = (col + j);
+				// Only search when on the board
+				if (this.isOnBoard(searchR, searchC) == true){
+
+					// See if we surround the opponent
+					if(this.board[searchR][searchC] == opponent) 
+						while (this.board[searchR][searchC] 
+							== opponent) {
+							// Continue the search in a straight line
+							if (i == -1 && j == -1) {
+								searchR--;
+								searchC--;
+							}
+							else if (i == 0 && j == -1) {
+								searchC--;
+							}
+							else if (i == 0 && j == 1) {
+								searchC++;
+							}
+							else if (i == 1 && j == -1) {
+								searchR++;
+								searchC--;
+							}
+							else if (i == -1 && j == 0) {
+								searchR--;
+							}
+							else if (i == 1 && j == 0) {
+								searchR++;
+							}
+							else if (i == -1 && j == 1) {
+								searchR--;
+								searchC++;
+							}
+							else if (i == 1 && j == 1) {
+								searchR++;
+								searchC++;
+							}
+
+							// Check if this new space is ours
+							// Return true if it is
+							if (this.isOnBoard(searchR, searchC)
+								== true){
+								if (this.board[searchR][searchC]
+									== disc) {
+									return true;
+								}
+							}
+							else
+								break;	
+							
+						}
+				}
+			}
+		return false;
+	}
+
+	/**
+	 * placeDiscAt
+	 * @param row An integer number for row.
+	 * @param col An integer number for column.
+	 * @param disc A character standing for disc color.
+	 */
+	placeDiskAt(row, col, disc){
+		var tempR, tempC;
+		var permR = row;
+		var permC = col;
+		//Goes through all of the difference changes
+		for(let rChange = -1; rChange < 2; rChange++){
+			for(let cChange = -1; cChange < 2; cChange++){
+				tempR = rChange + permR;
+				tempC = cChange + permC;
+
+				//Make sure it's not a blank space
+				//or the current player
+				if(this.isOnBoard(tempR, tempC) == true){
+					while(this.board[tempR][tempC] != -1 &&
+						this.board[tempR][tempC] != disc){
+						//increment the pieces past that piece
+						tempR += rChange;
+						tempC += cChange; 
+
+						//check if the piece is equal
+						//to the current player
+						if(this.isOnBoard(tempR, tempC) == true){
+							if(this.board[tempR][tempC] == disc){
+								row = permR;
+								col = permC;
+
+								for(;tempR != row || tempC != col;
+									col += cChange, row += rChange){
+									if(this.isOnBoard(tempR, tempC)
+										== true)
+										this.board[row][col] = disc;
+								}
+								break;
+							}
+						}
+						if(this.isOnBoard(tempR, tempC) == false)
+							break;
+					}	
+				}
+			}
+		}
+
+		if(this.isOnBoard(permR, permC) == true)
+			this.board[permR][permC] = disc;
+	}
+
+	/**
+	 * isValidMoveAvailable
+	 * @param disc A character pertaining to a disc color.
+	 * @return bool A boolean telling the user whether there are
+	 *	 	valid moves availabe for that disc.
+	 */
+	isValidMoveAvailable(disc){
+		// Look at empty spaces for valid moves.
+		for (let i=0; i<this.height; i++)
+			for (let j=0; j<this.width; j++)
+				if (this.board[i][j] == -1)
+					if(this.isValidMove( i, j, disc) == true)
+						return true;
+
+		return false;
+	}
+
+
+	/**
+	 * checkWinner
+	 * @return char Which player has won.  Return null if
+	 * 		a tie exists.
+	 */
+	checkWinner(){
+		// Store the nmber of white and black chips separately.
+		var white = 0;
+		var black = 0;
+
+		// Check every board space for chips and increment by color.
+		for (let i=0; i<this.height; i++)
+			for (let j=0; j<this.width; j++)
+				if (this.board[i][j] == 0)
+					white++;
+				else if (this.board[i][j] == 1)
+					black++;
+
+		// The higher chips of a color wins.
+		if (white > black)
+			return 0;
+		else if (black > white)
+			return 1;
+		else
+			return -1;
+	}
+
+	//*****************************************************************
+	// HELPER FUNCTION: Checks if a move is on the board.
+	// @param r row location
+	// @param c column location
+	// @return true move is on the board
+	// @return false move is not on the board
+	//*****************************************************************
+	isOnBoard(r, c) {
+		if (r >= this.height|| r < 0)
+			return false;
+		if (c >= this.width || c < 0)
+			return false;
+		return true;
+	}
+
+}
+
+//let board = new Board(10, 10);
+//board.printBoard();
---
+++ main.js
@@ -0,0 +1,122 @@
+/**
+ * Othello
+ * Javascript project for CIS 343.
+ * Command-line version of Othello.
+ */
+
+// Import our board definitions
+const board = require('./board.js');
+// Import a synchronous prompt library
+const prompt = require('prompt-sync')();
+
+/**
+ * saveFile
+ * SYNCHRONOUS (blocking) file save function.
+ * @param file - The full filename path we want to save to.
+ * @param contents - The object we wish to save as a JSON file.
+ */
+function saveFile(file, contents){
+	let fs = require('fs');
+	fs.writeFileSync(file, JSON.stringify(contents));
+}
+
+/**
+ * loadFile
+ * SYNCHRONOUS (blocking) file read function.
+ * @param file - The full filename path we wish to load an object from.
+ * @return contents - The object converted from JSON.
+ */
+function loadFile(file){
+	const fs = require('fs');
+	let myData = fs.readFileSync('test.json');
+	let game = JSON.parse(myData);
+	console.log(game);
+	return game;
+}
+
+/**
+ * Driver function.  "main" method, if you will.
+ */
+function start(){
+	// Local variables
+	let data = loadFile('test.json');	
+	do{
+		var inp = Number(prompt('Enter 1 to load file or 2 to start new game: '));
+	}while(inp != 2 && inp != 1);
+	var myR = 0;
+	var myC = 0;
+	if(inp == 2){
+
+		do{
+			myR = Number(prompt('Number of rows: '));
+			myC = Number(prompt('Number of columns: '));
+			if(myR < 4 || myC < 4 || myR > 12 || myC > 12)
+				console.log('Input must be greater than 3 or less than 13');}
+		while(myR < 4 || myC < 4 || myR > 12 || myC > 12);
+	}
+	else{
+		myR = data.height;
+		myC = data.width;
+	}
+
+	var myBoard = new board(myR, myC);
+	if(inp == 1)
+		myBoard.setBoard(data.board);
+	// SYNCHRONOUSLY read from keyboard
+	console.log('Creating a board with size ' + myR + ' x ' + myC + '.');
+
+
+	var col = 0;
+	var row = 0;
+	var currPlyr = 1;
+	// Print board
+	while(myBoard.isGameOver() == false){
+		console.log('\n\n');
+		myBoard.printBoard();
+		if(myBoard.isValidMoveAvailable(currPlyr) == false){
+			console.log('No valid moves availables for player ' + currPlyr + '. You lose your turn.\n');
+			currPlyr = (currPlyr + 1) % 2;
+		}
+		console.log('Player ' + currPlyr);
+		row = Number(prompt('Enter row (Enter -1 to exit program immediately or -2 to save & exit): '));
+		if(row == -1){
+			console.log('Exiting');
+			process.exit();
+		}
+		if(row == -2){
+			console.log('Saving & exiting');
+			break;
+		}
+		col = Number(prompt('Enter col: '));
+		col--;
+		row--;
+
+		if(col >= data.height || col < 0 || row >= data.height || row < 0)
+			console.log('Please enter valid coordinates');
+		else if(!myBoard.isValidMove(row,col,currPlyr)){
+			console.log('Sorry, not a valid move');
+		}
+		else{
+			myBoard.placeDiskAt(row,col,currPlyr);	
+			currPlyr = (currPlyr + 1) % 2;
+		}
+
+	}
+
+	if(row == -2){
+		saveFile('test.json', myBoard);
+	}
+	else{
+		myBoard.printBoard();
+		if(myBoard.checkWinner() == 1)
+			console.log("Player 1 won!");
+		else if(myBoard.checkWinner() == 0)
+			console.log("Player 0 won!");
+		else
+			console.log("It was a tie");
+	}
+
+}
+
+console.clear();
+start();
